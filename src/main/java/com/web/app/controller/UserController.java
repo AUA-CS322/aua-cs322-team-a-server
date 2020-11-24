0dbee184f00d4d39d098978fad5b805c97d7e536
MagaZargaryan <margarita_zargaryan@edu.aua.am>
2020-11-24 13:14:01 +0400

    fixes

--- src/main/java/com/web/app/controller/UserController.java
+++ src/main/java/com/web/app/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
